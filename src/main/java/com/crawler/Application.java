2deb6d58cd9a615bdfd147f63ac30b65025c0ebb
Artur Krochin <arturkrochin@Arturs-MacBook-Pro.local>
2021-01-24 00:42:02 +0000

    introduce logging

--- src/main/java/com/crawler/Application.java
+++ src/main/java/com/crawler/Application.java
@@ -5,6 +5,8 @@ import com.crawler.client.UnirestClient;
 import com.crawler.parser.JsoupAnchorLinkParser;
 import com.crawler.validator.SameDomainLinkPolicy;
 import com.crawler.webcrawler.WebCrawler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.List;
@@ -13,35 +15,60 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Application {
 
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class.getName());
+
+    // TODO: static factory?
+    // TODO: proper exception handling
+    // TODO: performance testing
+    // TODO: put this in Argparse4j
+    // TODO: finish configuring log4j
     public static void main(String[] args) throws Exception {
+        logger.info("Starting web crawler");
+
+        var concurrencyLevel = 128;
+        var startingLink = "https://www.baeldung.com/java-concurrent-map";
+
         var visitedPaths = new ConcurrentHashMap<String, URI>();
         var pathQueue = new ConcurrentLinkedQueue<URI>();
-        pathQueue.add(new URI("https://www.baeldung.com/java-concurrent-map"));
+        pathQueue.add(new URI(startingLink));
 
+        Runnable crawlingTask =
+                () -> {
+                    var crawler =
+                            WebCrawler.builder()
+                                    .client(
+                                            UnirestClient.fromConfig(
+                                                    ReliabilityConfig.builder()
+                                                            .connectionTimeoutMillis(500)
+                                                            .build()))
+                                    .parser(JsoupAnchorLinkParser.newInstance())
+                                    .linkPolicies(List.of(SameDomainLinkPolicy.newInstance()))
+                                    .visitedPaths(visitedPaths)
+                                    .pathQueue(pathQueue)
+                                    .build();
 
-        ExecutorService executorService =
-                Executors.newFixedThreadPool(4);
+                    crawler.crawl();
+                };
 
+        ExecutorService executorService = getExecutorService(concurrencyLevel);
 
-        // TODO: static factory?
-        // TODO: proper exception handling
-        // TODO: performance testing
-        // TODO: add ExecutorService and multithreading and the rest
-        var crawler =
-                WebCrawler.builder()
-                        .client(
-                                UnirestClient.fromConfig(
-                                        ReliabilityConfig.builder()
-                                                .connectionTimeoutMillis(500)
-                                                .build()))
-                        .parser(JsoupAnchorLinkParser.newInstance())
-                        .linkPolicies(List.of(SameDomainLinkPolicy.newInstance()))
-                        .visitedPaths(visitedPaths)
-                        .pathQueue(pathQueue)
-                        .build();
+        for (int i = 0; i < concurrencyLevel; i++) {
+            executorService.submit(crawlingTask);
+        }
+    }
 
-        crawler.crawl();
+    private static ExecutorService getExecutorService(int concurrencyLevel) {
+        ExecutorService executorService = Executors.newFixedThreadPool(concurrencyLevel);
+        Runtime.getRuntime()
+                .addShutdownHook(
+                        new Thread(
+                                () -> {
+                                    logger.info("Shutting down executor service");
+                                    executorService.shutdown();
+                                }));
+
+        return executorService;
     }
 }
--- src/main/java/com/crawler/webcrawler/WebCrawler.java
+++ src/main/java/com/crawler/webcrawler/WebCrawler.java
@@ -1,9 +1,12 @@
 package com.crawler.webcrawler;
 
+import com.crawler.Application;
 import com.crawler.client.Client;
 import com.crawler.parser.LinkParser;
 import com.crawler.validator.LinkSelectionPolicy;
 import lombok.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.List;
@@ -13,6 +16,8 @@ import java.util.stream.Collectors;
 
 @Builder
 public class WebCrawler {
+    private static Logger logger = LoggerFactory.getLogger(WebCrawler.class.getName());
+
     private final Client client;
     private final LinkParser parser;
     private final List<LinkSelectionPolicy> linkPolicies;
@@ -20,19 +25,22 @@ public class WebCrawler {
     private final Queue<URI> pathQueue;
 
     public void crawl() {
-        var startingLink = pathQueue.poll();
-        var body = client.fetchBody(startingLink);
-        var links = parser.parseLinks(body);
-        var validLinks =
-                links.stream()
-                        .filter(link -> isAllPoliciesPass(startingLink, link))
-                        .filter(link -> !isPreviouslyVisitedPath(visitedPaths, link.getPath()))
-                        .collect(Collectors.toList());
+        logger.info("Thread {} is operational", Thread.currentThread().getId());
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            var startingLink = pathQueue.poll();
+            var body = client.fetchBody(startingLink);
+            var links = parser.parseLinks(body);
+            var validLinks =
+                    links.stream()
+                            .filter(link -> isAllPoliciesPass(startingLink, link))
+                            .filter(link -> !isPreviouslyVisitedPath(visitedPaths, link.getPath()))
+                            .collect(Collectors.toList());
 
-        System.out.println(links);
-        System.out.println(validLinks);
+            visitedPaths.put(startingLink.getPath(), startingLink);
 
-        visitedPaths.put(startingLink.getHost(), startingLink);
+            pathQueue.addAll(validLinks);
+        }
     }
 
     private boolean isAllPoliciesPass(URI startingLink, URI link) {
